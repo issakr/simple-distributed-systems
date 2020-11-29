b92fcce400ea08472ba6a065e1cdb096e71ab49f
Issam sakr <sakrissam@gmail.com>
2020-11-29 20:46:40 +0100

    no doc

--- Anzeigetafel-RMI/TafelClient/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"vs.anzeigetafel"},{"l":"vs.anzeigetafel.controller"},{"l":"vs.anzeigetafel.model"},{"l":"vs.anzeigetafel.service"},{"l":"vs.anzeigetafel.spring.config"},{"l":"vs.anzeigetafel.view"},{"l":"vs.fullClient"}]
--- Anzeigetafel-RMI/TafelServer/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"vs.anzeigetafel"},{"l":"vs.anzeigetafel.model"},{"l":"vs.anzeigetafel.service"},{"l":"vs.anzeigetafel.spring.config"},{"l":"vs.fullClient"}]
